d1ef6c907eb840c8faaf3326a3d138161390c90b
Ajeet Singh <ajeetsinghtomer@gmail.com>
2020-05-10 14:15:10 +0000

    run app added

--- base/libs/build_app.bat
+++ base/libs/build_app.bat
@@ -1,13 +1,12 @@
 @ECHO OFF
 REM ##### If you dont want to display running commands than just change ECHO ON to ECHO OFF
 
-SET COPIED_APP_PATH=C:/copied-app
-SET BUNDLE_DIR=C:/tmp/bundle-dir
+SET APP_DIR=c:\app
+SET BUNDLE_DIR=C:\tmp\bundle-dir
 
 REM ### Just to make sure that meteor is in PATH
 setx /M Path "%LocalAppData%\\.meteor;%Path%"
 
-mkdir C:\copied-app
 mkdir C:\tmp\bundle-dir
 
 if "%errorlevel%"=="1" (
@@ -15,28 +14,11 @@ if "%errorlevel%"=="1" (
 	GOTO END
 )
 
-ECHO "=>########## Assuming that your app already in C:/app directory"
-ECHO "=> Copying the app to %COPIED_APP_PATH%"
-COPY "C:\app" "%COPIED_APP_PATH%"
-
-if "%errorlevel%"=="1" (
-	GOTO END
-)
-
-CD "%COPIED_APP_PATH%"
-ECHO .
+CD "%APP_DIR%"
 ECHO "=> Executing NPM install --production"
 meteor npm install --production
-
-if "%errorlevel%"=="1" (
-	GOTO END
-)
-
-mkdir c:\buzzybuildwindows
-
-ECHO "=> Executing Meteor Build..."
-meteor build ../buzzybuildwindows --server-only --allow-superuser --directory "%BUNDLE_DIR%" --server=http://localhost:3000 
-REM #### meteor build ../buzzybuildwindows --server-only --architecture os.windows.x86_64
+ECHO "=> Executing meteor build"
+meteor build "%BUNDLE_DIR%" --server-only --architecture os.windows.x86_64
 
 if "%errorlevel%"=="1" (
 	GOTO END
@@ -73,12 +55,10 @@ if "%errorlevel%"=="1" (
 
 ECHO "=> Cleaning up"
 # cleanup
-ECHO " => Deleting %COPIED_APP_PATH%"
-rmdir /S /Q $COPIED_APP_PATH
 ECHO " => BUNDLE_DIR"
 rmdir /S /Q %BUNDLE_DIR%
-ECHO " => Deleting meteor => %LocalAppData%"
-rmdir /S /Q "%LocalAppData%"
+ECHO " => Deleting meteor => %LocalAppData%\\.meteor"
+rmdir /S /Q "%LocalAppData%\\.meteor"
 
 :END
 REM ECHO "Completed with code %errorlevel%"
--- base/libs/run_app.bat
+++ base/libs/run_app.bat
@@ -1,3 +1,30 @@
 @ECHO OFF
 
-ECHO "========================================>"
+if "%1%"=="-d" (
+	GOTO :BUNDLE
+)
+
+ECHO "=> You don't have an meteor app to run in this image."
+GOTO :END
+
+:BUNDLE
+  if "%2%" == "/built_app" (
+     ECHO "=> Using bundle from c:\built_app"
+     cd c:\built_app
+	 GOTO :START_APP
+  )
+  GOTO :END
+
+:START_APP
+  SET PORT=80
+
+  ECHO "=> Starting meteor app on port:$PORT"
+  node "%METEORD_NODE_OPTIONS%" main.js
+  GOTO :END
+  
+:END
+REM ECHO "Completed with code %errorlevel%"
+  if "%errorlevel%"=="1" (
+	  echo "Failed"
+  )
+  EXIT /B "%errorlevel%"
